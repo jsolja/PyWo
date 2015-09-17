a8495e1614d672270560ed6f2a2618e0b142efd0
jursolja <jurasolja@gmail.com>
2015-09-17 11:18:32 +0200

    update

--- rr.py
+++ rr.py
@@ -14,7 +14,6 @@ class Tocka:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        pass
     
     def norm(self):
         return sqrt(self.x**2 + self.y**2)
@@ -37,8 +36,16 @@ class Vektor(Tocka):
         y = self.y - b.y
         return Vektor(x, y)
     
+    def __mul__(self, b):
+        if(type(b) == type(self)):
+            return self.x * b.x + self.y * b.y
+        else:
+            return Vektor(self.x * b, self.y * b)
+    
+    __rmul__ = __mul__
+        
     def norm(self):
         return sqrt(self.x**2 + self.y**2)
     
     def __repr__(self):
-        return "Tocka({0}, {1})".format(self.x,self.y)
+        return "Vektor({0}, {1})".format(self.x,self.y)
